0ae1c682e2b66779fed509f2d6ac8e0775fca607
kvrovsky5 <kvrovsky3@gmail.com>
2024-04-06 09:35:28 +0200

    zad2

--- pp-lab-3/main.java
+++ pp-lab-3/main.java
@@ -1,8 +1,12 @@
 
-public class Main {
+public class main {
 
+    
     public static void main(String[] args) {
         Point point = new Point(2.5, 3.14);
+        Point center = new Point(2.5, 3.14);
+        Circle circle = new Circle(center, 5.0);
+        
 
         System.out.println("Współrzędne punktu:");
         System.out.println("x = " + point.getX());
@@ -14,5 +18,13 @@ public class Main {
         System.out.println("\nNowe współrzędne punktu:");
         System.out.println("x = " + point.getX());
         System.out.println("y = " + point.getY());
+
+        System.out.println("--------------------------");
+        System.out.println("Właściwości koła:");
+        System.out.println("Środek: " + circle.getCenter());
+        System.out.println("Promień: " + circle.getRadius());
+        System.out.println("Obwód: " + circle.calculatePerimeter());
+
+        
     }
 }
