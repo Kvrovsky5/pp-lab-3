55d09e684a0dc0d433ae59a2f2e9368a9c426c62
kvrovsky5 <kvrovsky3@gmail.com>
2024-04-06 10:11:31 +0200

    zad5

--- pp-lab-3/main.java
+++ pp-lab-3/main.java
@@ -3,21 +3,15 @@
 import geometry.Circle;
 import geometry.ColoredCircle;
 import geometry.Point;
+import geometry.Rectangle;
 
 public class main {
 
     public static void main(String[] args) {
-        Circle[] circles = new Circle[3];
-        circles[0] = new Circle(new Point(2.5, 3.14), 5.0);
-        circles[1] = new ColoredCircle(new Point(1.0, 2.0), 3.0, "Czerwony");
-        circles[2] = new Circle(new Point(4.0, 5.0), 7.0);
+        Rectangle rectangle = new Rectangle(5.0, 3.0);
 
-        for (Circle circle : circles) {
-            System.out.println("Pole powierzchni: " + circle.calculateArea());
-            if (circle instanceof ColoredCircle) {
-                ColoredCircle coloredCircle = (ColoredCircle) circle;
-                System.out.println("Kolor: " + coloredCircle.getColor());
-            }
-        }
+        System.out.println(rectangle); 
+        System.out.println("Pole powierzchni: " + rectangle.calculateArea()); 
+        System.out.println("Obwód: " + rectangle.calculatePerimeter()); 
     }
 }
