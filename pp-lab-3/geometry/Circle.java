b7cc0b455b20510564267cbad93faf0ecc18d19b
kvrovsky5 <kvrovsky3@gmail.com>
2024-04-06 10:04:24 +0200

    zad4

--- pp-lab-3/geometry/Circle.java
+++ pp-lab-3/geometry/Circle.java
@@ -4,6 +4,9 @@ public class Circle {
     private Point center;
     private double radius;
 
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
     public Circle(Point center, double radius) {
         this.center = center;
         this.radius = radius;
--- pp-lab-3/main.java
+++ pp-lab-3/main.java
@@ -7,10 +7,17 @@ import geometry.Point;
 public class main {
 
     public static void main(String[] args) {
-        Point center = new Point(2.5, 3.14);
-        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "Czerwony");
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(new Point(2.5, 3.14), 5.0);
+        circles[1] = new ColoredCircle(new Point(1.0, 2.0), 3.0, "Czerwony");
+        circles[2] = new Circle(new Point(4.0, 5.0), 7.0);
 
-        System.out.println("Właściwości kolorowego koła:");
-        System.out.println(coloredCircle);
+        for (Circle circle : circles) {
+            System.out.println("Pole powierzchni: " + circle.calculateArea());
+            if (circle instanceof ColoredCircle) {
+                ColoredCircle coloredCircle = (ColoredCircle) circle;
+                System.out.println("Kolor: " + coloredCircle.getColor());
+            }
+        }
     }
 }
